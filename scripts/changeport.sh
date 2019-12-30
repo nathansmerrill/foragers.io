04a8a163284bd704dad6f56d143d6600cc714703
Nathan Merrill <mathiscool3000@gmail.com>
2019-12-30 16:56:39 +0000

    Switch to apache for serving frontend

--- scripts/changeport.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-PROJECT="/home/merrilln/foragers.io"
-cd $PROJECT
-sed -i 's/port=4000/port=8020/' server.py
-sed -i 's/localhost:4000/cs.catlin.edu:8020/' public/sketch.js
---
+++ scripts/serverify.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#PROJECT="/home/merrilln/foragers.io"
+PROJECT="/home/nathan/Documents/Websites/foragers.io"
+cd $PROJECT
+sed -i 's/port=4000/port=8020/' server.py
+sed -i "s/app.router.add_get('\/', index)/# app.router.add_get('\/', index)/" server.py
+sed -i "s/app.router.add_static('\/', '.\/public')/# app.router.add_static('\/', '.\/public')/" server.py
+sed -i 's/localhost:4000/cs.catlin.edu:8020/' public/sketch.js
